6cf2b6f442ac8a2d94225329e225524e36ec4957
quan.chen <quan.chen@sumscope.com>
2018-03-08 18:46:23 +0800

    begin to impl epoll

--- qkc/sys_epoll.cpp
+++ qkc/sys_epoll.cpp
@@ -2,10 +2,18 @@
 #include <sys/epoll.h>
 #include <wintf/wobj.h>
 #include <windows.h>
+#include <errno.h>
 
 int epoll_create (int size)
 {
-    return 0 ;
+    HANDLE handle = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE , NULL , 0 , 0) ;
+    if(handle == INVALID_HANDLE_VALUE)
+    {
+        errno = EIO ;
+        return -1 ;
+    }
+
+    return wobj_set(WOBJ_IOCP , handle , NULL) ;
 }
 
 int epoll_ctl (int epfd, int op, int fd, struct epoll_event * ev)
